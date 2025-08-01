4f266ffa3a4cb6073f5753f8d2bd99cba22978b7
Ian Brown <ibapps39@gmail.com>
2025-07-31 22:04:21 -0600

    Started constants, entry points

--- include/chip8.h
+++ include/chip8.h
@@ -1,6 +1,9 @@
-#include <cstdint>
+#include <cstdint> // unint8_t, uint16_t, etc..
 
-#define MEM_SIZE
+#define DEFAULT_MEM_SIZE 4096 // bytes
+#define DEFAULT_WIDTH 64
+#define DEFAULT_HEIGHT 32
+#define DEFAULT_REGISTER_STACK_SIZE 16
 
 class chip8
 {
@@ -8,10 +11,15 @@ private:
     /* data */
     ;
 public:
-    uint16_t stack[16];
-    uint16_t PC;
-    uint8_t SP;
-    uint8_t memory[4096];
+    const int MEM_SIZE = DEFAULT_MEM_SIZE;
+    const int DISPLAY_WIDTH = DEFAULT_WIDTH;
+    const int DISPLAY_HEIGHT = DEFAULT_HEIGHT;
+    const uint16_t PROG_START = 0x200;
+
+    uint16_t stack[DEFAULT_REGISTER_STACK_SIZE];
+    uint16_t PC = PROG_START;
+    uint8_t SP = ;
+    uint8_t MEM_SIZE[DEFAULT_MEM_SIZE];
     chip8() : PC(0x200), SP(0);
     ~chip8();
 };
