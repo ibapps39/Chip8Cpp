403bad026eea8d8fc98a0cfd6edf472bbf9dfb89
Ian Brown <ibapps39@gmail.com>
2025-08-12 09:14:49 -0600

    implemented opcodes 1nnn, 2nnn, 3xkk, 4xkk. Started 5xy0. Updated .gitignore to include/ignore book

--- include/chip8.h
+++ include/chip8.h
@@ -1,6 +1,8 @@
 #include <cstdint> // unint8_t, uint16_t, etc..
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <random>
 
 #define DEFAULT_MEM_SIZE 4096 // bytes
 #define DEFAULT_WIDTH 64
@@ -11,6 +13,8 @@ class chip8
 {
 private:
     /* data */
+    std::default_random_engine rng;
+    std::uniform_int_distribution<uint8_t> randByte;
 
 public:
     // static constexpr to avoid wasting memory, allocate to the class not the instance
@@ -36,7 +40,7 @@ public:
     static constexpr uint16_t STORAGE_START = 0x050;
     static constexpr uint16_t STORAGE_END = 0x0A0;
 
-    uint8_t stack_pointer = 0;     // 8bit Stack pointer
+    uint8_t sp = 0;     // 8bit Stack pointer
     uint8_t memory[MEM_SIZE] = {}; // Memory, RAM, array
     uint8_t v_registers[16] = {};  // 8bit General purpose registers array, Vx
     uint8_t delay_timer = 0;
@@ -44,7 +48,7 @@ public:
     uint8_t keypad[16] = {};
 
     uint16_t stack[REGISTER_STACK_SIZE] = {};
-    uint16_t prog_counter = DATA_START; // Program counter
+    uint16_t pc = DATA_START; // Program counter
     uint16_t index = {};                // "I", index, register stores memory address
     uint16_t opcode;
 
@@ -74,11 +78,27 @@ public:
     ~chip8() = default;
 
     void LoadROM(char const *filename) {};
+    // OPCODES
+    void OP_00E0(){};
+    void OP_00EE(){};
+    void OP_1nnn(){};
+    void OP_2nnn(){};
+    void OP_3xkk(){};
+    void OP_4xkk(){};
+    void OP_5xy0(){};
 };
-
-chip8::chip8(/* args */)
+chip8::chip8(/* args */) : rng(std::chrono::system_clock::now().time_since_epoch().count())
 {
-    prog_counter = DATA_START;
+    // Start Program
+    pc = DATA_START;
+    // Load fonts
+    for (unsigned int i = 0; i < sizeof(FONT_SET); ++i)
+    {
+        memory[FONT_START+i] = FONT_SET[i];
+    }
+
+    // random byte via rng -> system_clock
+    randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
 }
 
 chip8::~chip8()
@@ -97,7 +117,7 @@ void chip8::LoadROM(char const *filename)
     std::streampos size = file.tellg();
     char *buffer = new char[size];
 
-    file.seekg(0, std::ios::beg);
+    file.seekg(0, std::ios::beg); // move file (ptr) to beginning of file.
     file.read(buffer, size);
     file.close();
 
@@ -105,110 +125,51 @@ void chip8::LoadROM(char const *filename)
     {
         chip8::memory[DATA_START + i] = buffer[i];
     }
+
     delete[] buffer;
 }
 
-// #include <cstdint> // unint8_t, uint16_t, etc..
-// #include <fstream>
-// #include <iostream>
+// OP CLS, clear screen.
+void chip8::OP_00E0()
+{
+    memset(chip8::video, 0, sizeof(chip8::video));
+}
 
-// #define DEFAULT_MEM_SIZE 4096 // bytes
-// #define DEFAULT_WIDTH 64
-// #define DEFAULT_HEIGHT 32
-// #define DEFAULT_REGISTER_STACK_SIZE 16
+// OP RET, return from sub-routine.
+void chip8::OP_00EE()
+{
+    --chip8::sp;
+    chip8::pc = chip8::stack[sp];
+}
 
-// class chip8
-// {
-// private:
-//     /* data */
-
-// public:
-//     // static constexpr to avoid wasting memory, allocate to the class not the instance
-
-//     static constexpr int MEM_SIZE = DEFAULT_MEM_SIZE;
-//     static constexpr int DISPLAY_WIDTH = DEFAULT_WIDTH;
-//     static constexpr int DISPLAY_HEIGHT = DEFAULT_HEIGHT;
-//     static constexpr int REGISTER_STACK_SIZE = DEFAULT_REGISTER_STACK_SIZE;
-
-//     static constexpr uint16_t RESERVED_START = 0x000; // Memory starting address, reserved for interpreter
-//     static constexpr uint16_t RESERVED_END = 0x1FF; // Memory ending address, reserved for interpreter
-
-//     static constexpr uint16_t MEM_START = 0x000;
-//     static constexpr uint16_t MEM_END = 0xFFF;
-
-//     static constexpr uint16_t DATA_START = 0x200;     // Data space min
-//     static constexpr uint16_t DATA_END = 0xFFF;       // Data space max
-//     static constexpr uint16_t DATA_ETI_START = 0x600; // (alt.) Data space
-
-//     static constexpr uint16_t FONT_START = 0x050;
-//     static constexpr uint16_t FONT_END = 0x09F;
-
-//     static constexpr uint16_t STORAGE_START = 0x050;
-//     static constexpr uint16_t STORAGE_END = 0x0A0;
-
-//     uint8_t stack_pointer = 0;                        // 8bit Stack pointer
-//     std::array<uint8_t, MEM_SIZE> memory = {}; // Memory, RAM, array
-//     std::array<uint8_t, 16> v_registers = {};                    // 8bit General purpose registers array, Vx
-//     uint8_t delay_timer = 0;
-//     uint8_t sound_timer = 0;
-//     std::array<uint8_t, 16>keypad = {};
-
-//     std::array<uint16_t, REGISTER_STACK_SIZE> stack = {};
-//     uint16_t prog_counter = DATA_START; // Program counter
-//     uint16_t index = {};           // "I", index, register stores memory address
-//     uint16_t opcode;
-
-//     uint32_t video[DISPLAY_HEIGHT*DISPLAY_WIDTH] = {};
-
-//     static constexpr uint8_t FONT_SET[80] = { // Fonts, 15 5bit characters
-//         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-//         0x20, 0x60, 0x20, 0x20, 0x70, // 1
-//         0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-//         0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-//         0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-//         0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-//         0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-//         0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-//         0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-//         0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-//         0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-//         0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-//         0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-//         0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-//         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-//         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-//     };
-
-//     chip8() = default;
-//     ~chip8() = default;
-
-//     void LoadROM(char const* filename){};
-// };
-
-// chip8::chip8(/* args */)
-// {
-// }
-
-// chip8::~chip8()
-// {
-// }
-
-// // Loads ROM as stream of binary to buffer and copies to the start of chip8 memory/ram.
-// void chip8::LoadROM(char const* filename)
-// {
-//     std::ifstream file(filename, std::ios::binary | std::ios::ate); // std::ios::ate sets to end of stream
-
-//     if (!file.is_open()){
-//         std::clog << "Could not open file or no file is loaded!\n";
-//         return;
-//     }
-//     std::streampos size = file.tellg();
-
-//     // if size is zero...
-
-//     file.seekg(0, std::ios::beg);
-//     std::vector<char> buffer(size);
-
-//     std::copy(buffer.begin(), buffer.end(), &chip8::memory[DATA_START]);
-
-// }
+// OP JP addr, jump to address nnn via bitmask.
+void chip8::OP_1nnn()
+{
+    // assign the program counter to the lower 12 bits
+    chip8::pc = chip8::opcode & 0x0FFFu;
+}
+// OP CALL addr, call subroutine and return.
+void chip8::OP_2nnn()
+{
+    chip8::stack[chip8::sp] = chip8::pc; // Push current pc before JP
+    ++chip8::sp; // Move sp up, for 00EE
+    chip8::pc = chip8::opcode & 0x0FFFu;
+}
+// OP SE Vx, kk. Skip(3) if Vx[x] == kk.
+void chip8::OP_3xkk()
+{
+    // uint8_t x = (chip8::opcode & 0x0F00u) >> 8u;
+    // uint8_t kk = chip8::opcode & 0x00FFu;
+    bool skip = chip8::v_registers[(chip8::opcode & 0x0F00u) >> 8u] == (chip8::opcode & 0x00FFu);
+    if(!skip) return;
+    chip8::pc += 2;
+}
+// OP SNE Vx, kk. Skip(4) if Vx[x] != kk. Opposite, similar, to OP_3xkk.
+void chip8::OP_4xkk()
+{
+    // uint8_t x = (chip8::opcode & 0x0F00u) >> 8u;
+    // uint8_t kk = chip8::opcode & 0x00FFu;
+    bool skip = chip8::v_registers[(chip8::opcode & 0x0F00u) >> 8u] != (chip8::opcode & 0x00FFu);
+    if(!skip) return;
+    chip8::pc += 2;
+}
