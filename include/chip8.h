bf51cdb47fadefa2fd1f8c27dbf3937320c54fc0
Ian Brown <ibapps39@gmail.com>
2025-08-16 10:54:45 -0600

    Finished opcodes, now starting function pointer table

--- include/chip8.h
+++ include/chip8.h
@@ -40,6 +40,19 @@ public:
     static constexpr uint16_t STORAGE_START = 0x050;
     static constexpr uint16_t STORAGE_END = 0x0A0;
 
+    struct Registers // 8bit General purpose registers array, Vx
+    {
+        uint16_t I; // "I", index, register stores memory address
+        union
+        {
+            uint8_t V[16];
+            struct
+            {
+                uint8_t V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF;
+            };
+        };
+    } registers;
+
     uint8_t sp = 0;                // 8bit Stack pointer
     uint8_t memory[MEM_SIZE] = {}; // Memory, RAM, array
     uint8_t v_registers[16] = {};  // 8bit General purpose registers array, Vx
@@ -99,6 +112,21 @@ public:
     void OP_8xy7() {};
     void OP_8xyE() {};
     void OP_9xy0() {};
+    void OP_Annn() {};
+    void OP_Bnnn() {};
+    void OP_Cxkk() {};
+    void OP_Dxyn() {};
+    void OP_Ex9E() {};
+    void OP_ExA1() {};
+    void OP_Fx07() {};
+    void OP_Fx0A() {};
+    void OP_Fx15() {};
+    void OP_Fx18() {};
+    void OP_Fx1E() {};
+    void OP_Fx29() {};
+    void OP_Fx33() {};
+    void OP_Fx55() {};
+    void OP_Fx65() {};
 };
 chip8::chip8(/* args */) : rng(std::chrono::system_clock::now().time_since_epoch().count())
 {
@@ -112,6 +140,12 @@ chip8::chip8(/* args */) : rng(std::chrono::system_clock::now().time_since_epoch
 
     // random byte via rng -> system_clock
     randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+
+    using Instruction = void(chip8::*)();
+    Instruction table[16] = 
+    {
+        &chip8::OP_1nnn
+    }
 }
 
 chip8::~chip8()
@@ -145,52 +179,52 @@ void chip8::LoadROM(char const *filename)
 // OP CLS, clear screen.
 void chip8::OP_00E0()
 {
-    memset(chip8::video, 0, sizeof(chip8::video));
+    memset(video, 0, sizeof(video));
 }
 
 // OP RET, return from sub-routine.
 void chip8::OP_00EE()
 {
-    --chip8::sp;
-    chip8::pc = chip8::stack[sp];
+    --sp;
+    pc = stack[sp];
 }
 
 // OP JP addr, jump to address nnn via bitmask.
 void chip8::OP_1nnn()
 {
     // assign the program counter to the lower 12 bits
-    chip8::pc = chip8::opcode & 0x0FFFu;
+    pc = opcode & 0x0FFFu;
     // NNN = 0000 1111 1111 11111 -> ()0000 (N)ibble (N)ibble (N)ibble
 }
 
 // OP CALL addr, call subroutine and return.
 void chip8::OP_2nnn()
 {
-    chip8::stack[chip8::sp] = chip8::pc; // Push current pc before JP
-    ++chip8::sp;                         // Move sp up, for 00EE
-    chip8::pc = chip8::opcode & 0x0FFFu;
+    stack[sp] = pc; // Push current pc before JP
+    ++sp;           // Move sp up, for 00EE
+    pc = opcode & 0x0FFFu;
 }
 
 // OP SE Vx, kk. Skip(3) if Vx[x] == kk. Where kk -> 0000 0000 1111 1111, an 8bit/byte immediate value
 void chip8::OP_3xkk()
 {
-    // uint8_t x = (chip8::opcode & 0x0F00u) >> 8u;
-    // uint8_t kk = chip8::opcode & 0x00FFu;
-    bool skip = chip8::v_registers[(chip8::opcode & 0x0F00u) >> 8u] == (chip8::opcode & 0x00FFu);
+    // uint8_t x = (opcode & 0x0F00u) >> 8u;
+    // uint8_t kk = opcode & 0x00FFu;
+    bool skip = v_registers[(opcode & 0x0F00u) >> 8u] == (opcode & 0x00FFu);
     if (!skip)
         return;
-    chip8::pc += 2;
+    pc += 2;
 }
 
 // OP SNE Vx, kk. Skip(4) if Vx[x] != kk. Opposite, similar, to OP_3xkk.
 void chip8::OP_4xkk()
 {
-    // uint8_t x = (chip8::opcode & 0x0F00u) >> 8u;
-    // uint8_t kk = chip8::opcode & 0x00FFu;
-    bool skip = chip8::v_registers[(chip8::opcode & 0x0F00u) >> 8u] != (chip8::opcode & 0x00FFu);
+    // uint8_t x = (opcode & 0x0F00u) >> 8u;
+    // uint8_t kk = opcode & 0x00FFu;
+    bool skip = v_registers[(opcode & 0x0F00u) >> 8u] != (opcode & 0x00FFu);
     if (!skip)
         return;
-    chip8::pc += 2;
+    pc += 2;
 }
 
 // 0000 0000 0000 0000
@@ -198,56 +232,56 @@ void chip8::OP_4xkk()
 // OP SE Vx, Vy. Skip if Vx = Vy
 void chip8::OP_5xy0()
 {
-    // uint8_t x = (chip8::opcode & 0x0F00u) >> 8u; // Vx
-    // uint8_t y = (chip8::opcode & 0x00F0u) >> 4u; // Vy
-    bool skip = chip8::v_registers[(chip8::opcode & 0x0F00u) >> 8u] == chip8::v_registers[(chip8::opcode & 0x00F0u) >> 4u];
+    // uint8_t x = (opcode & 0x0F00u) >> 8u; // Vx
+    // uint8_t y = (opcode & 0x00F0u) >> 4u; // Vy
+    bool skip = v_registers[(opcode & 0x0F00u) >> 8u] == v_registers[(opcode & 0x00F0u) >> 4u];
     if (!skip)
         return;
-    chip8::pc += 2;
+    pc += 2;
 }
 
 // OP LD Vx, kk
 void chip8::OP_6xkk()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] = (chip8::opcode & 0x00FFu);
+    v_registers[(opcode & 0x0F00u) >> 8u] = (opcode & 0x00FFu);
 }
 
 // OP ADD Vx, kk
 void chip8::OP_7xkk()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] += (chip8::opcode & 0x00FFu);
+    v_registers[(opcode & 0x0F00u) >> 8u] += (opcode & 0x00FFu);
 }
 
 // OP LD Vx, Vy
 void chip8::OP_8xy0()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] = v_registers[(chip8::opcode & 0x00F0u) >> 4u];
+    v_registers[(opcode & 0x0F00u) >> 8u] = v_registers[(opcode & 0x00F0u) >> 4u];
 }
 
 // OP OR Vx, Vy
 void chip8::OP_8xy1()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] |= v_registers[(chip8::opcode & 0x00F0u) >> 4u];
+    v_registers[(opcode & 0x0F00u) >> 8u] |= v_registers[(opcode & 0x00F0u) >> 4u];
 }
 
 // OP AND Vx, Vy
 void chip8::OP_8xy2()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] &= v_registers[(chip8::opcode & 0x00F0u) >> 4u];
+    v_registers[(opcode & 0x0F00u) >> 8u] &= v_registers[(opcode & 0x00F0u) >> 4u];
 }
 
 // OP XOR Vx, Vy
 void chip8::OP_8xy3()
 {
-    v_registers[(chip8::opcode & 0x0F00u) >> 8u] ^= v_registers[(chip8::opcode & 0x00F0u) >> 4u];
+    v_registers[(opcode & 0x0F00u) >> 8u] ^= v_registers[(opcode & 0x00F0u) >> 4u];
 }
 
 // OP ADD Vx, Vy. VF = carry.
 void chip8::OP_8xy4()
 {
     // Vx, mask out x and shift to make it 8 bit 0000 0000 0000 0000
-    uint8_t Vx = (chip8::opcode & 0x0F00u) >> 8u;
-    uint8_t Vy = (chip8::opcode & 0x00F0u) >> 4u;
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
     // uint8_t Vf = 0xF;
 
     uint16_t sum = v_registers[Vx] + v_registers[Vy];
@@ -262,8 +296,8 @@ void chip8::OP_8xy4()
 // // OP SUB Vx, Vy
 void chip8::OP_8xy5()
 {
-    uint8_t Vx = (chip8::opcode & 0x0F00u) >> 8u;
-    uint8_t Vy = (chip8::opcode & 0x00F0u) >> 4u;
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
     // uint8_t Vf = 0xF;
 
     v_registers[0xF] = (Vx > Vy) ? 1 : 0;
@@ -274,16 +308,16 @@ void chip8::OP_8xy5()
 // OP SHR Vx, Vy. If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
 void chip8::OP_8xy6()
 {
-    //uint8_t Vx = (chip8::opcode & 0x0F00u) >> 8u;
-    v_registers[0xF] = v_registers[(chip8::opcode & 0x0F00u) >> 8u] & 0x1u;
+    // uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    v_registers[0xF] = v_registers[(opcode & 0x0F00u) >> 8u] & 0x1u;
     v_registers[0xF] >>= 1;
 }
 
 // OP SUBN Vx, Vy. Set Vx = Vy - Vx, set VF = NOT borrow.
 void chip8::OP_8xy7()
-{ 
-    uint8_t Vx = (chip8::opcode & 0x0F00u) >> 8u;
-    uint8_t Vy = (chip8::opcode & 0x00F0u) >> 4u;
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
     v_registers[0xF] = (Vy > Vx) ? 1 : 0;
     v_registers[Vx] = v_registers[Vy] - v_registers[Vx];
 }
@@ -291,15 +325,158 @@ void chip8::OP_8xy7()
 // OP SHL Vx, {, Vy}. Vx = Vx SHL 1, Shift left. Similar to OP_SHR
 void chip8::OP_8xyE()
 {
-    //uint8_t Vx = (chip8::opcode & 0x0F00u) >> 8u;
-    // Mask first bit, then isolate.
-    v_registers[0xF] = (v_registers[(chip8::opcode & 0x0F00u) >> 8u] & 0x80u) >> 7u;
+    // uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    //  Mask first bit, then isolate.
+    v_registers[0xF] = (v_registers[(opcode & 0x0F00u) >> 8u] & 0x80u) >> 7u;
     v_registers[0x0F00u] <<= 1;
 }
 // OP SNE Vx, Vy. Skip next instruction if Vx != Vy
 void chip8::OP_9xy0()
 {
-    bool skip = v_registers[(chip8::opcode & 0x0F00u) >> 8u] != v_registers[(chip8::opcode & 0x00F0u) >> 4u];
-    if (!skip) return;
-    chip8::pc += 2;
+    bool skip = v_registers[(opcode & 0x0F00u) >> 8u] != v_registers[(opcode & 0x00F0u) >> 4u];
+    if (!skip)
+        return;
+    pc += 2;
+}
+
+// OP LD I, addr. Set I = nnn
+void chip8::OP_Annn()
+{
+    chip8::index = opcode & 0x0FFFu;
+}
+
+// OP JP V0, addr. Jump to nnn + V0
+void chip8::OP_Bnnn()
+{
+    pc = v_registers[0] + (opcode & 0x0FFFu);
+}
+
+// OP RND Vx, byte. Set Vx to a random byte AND kk
+void chip8::OP_Cxkk()
+{
+    v_registers[(opcode & 0x0F00u) >> 8u] = randByte(chip8::rng) & (opcode & 0x00FFu);
+}
+
+// OP DRW Vx, Vy, nibble. Draw Sprite (starting from I) at (Vx, Vy), n = height, VF = collision
+void chip8::OP_Dxyn()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    uint8_t height = opcode & 0x000Fu; // n
+
+    uint8_t x_pos = v_registers[Vx] % DISPLAY_WIDTH;
+    uint8_t y_pos = v_registers[Vy] % DISPLAY_HEIGHT;
+
+    v_registers[0xF] = 0; // Initialize VF, collision
+
+    for (size_t row = 0; row < height; ++row)
+    {
+        uint8_t sprite_b = chip8::memory[chip8::index + row];
+        for (size_t col = 0; col < 8; ++col)
+        {
+            uint8_t spritePixel = sprite_b & (0x80 >> col);
+            uint32_t *screenPx = &video[(y_pos + row) * DISPLAY_WIDTH + (x_pos + col)];
+
+            if (spritePixel)
+            {
+                if (*screenPx == 0xFFFFFFFF)
+                    v_registers[0xF] = 1;
+
+                *screenPx ^= 0xFFFFFFFF;
+            }
+        }
+    }
+}
+
+// OP Ex9E - SKP Vx, skip next instruction if keypad presses Vx
+void chip8::OP_Ex9E()
+{
+    bool skip = keypad[v_registers[(opcode & 0x0F00u) >> 8u]];
+    if (!skip)
+        return;
+    pc += 2;
+}
+
+// ExA1 - SKNP Vx, skip next instruction if Vx not pressed
+void chip8::OP_ExA1()
+{
+    if (!keypad[v_registers[(opcode & 0x0F00u) >> 8u]])
+        return;
+    pc += 2;
+}
+
+// LD Vx, DT, Vx = delay timer
+void chip8::OP_Fx07()
+{
+    v_registers[(opcode & 0x0F00u) >> 8u] = delay_timer;
+}
+
+// LD Vx, K. Wait for keypress and store in Vx
+void chip8::OP_Fx0A()
+{
+    for (size_t key = 0; key < 16; ++key)
+    {
+        if (keypad[key])
+        {
+            v_registers[(opcode & 0x0F00u) >> 8u] = key;
+            return;
+        }
+    }
+    pc -= 2;
+}
+
+// LD DT, Vx
+void chip8::OP_Fx15()
+{
+    delay_timer = v_registers[(opcode & 0x0F00u) >> 8u];
+}
+
+// LD ST = Vx
+void chip8::OP_Fx18()
+{
+    sound_timer = v_registers[(opcode & 0x0F00u) >> 8u];
+}
+
+// ADD I, VX
+void chip8::OP_Fx1E()
+{
+    index = index + v_registers[(opcode & 0x0F00u) >> 8u];
+}
+
+// LF F, Vx
+void chip8::OP_Fx29()
+{
+    uint8_t digit = v_registers[(opcode & 0x0F00u) >> 8u];
+    index = FONT_START + (5 * digit);
+}
+
+// LD B, Vx B => Binary-Codede Decimal.  (floats truncated)
+void chip8::OP_Fx33()
+{
+    uint8_t val = v_registers[(opcode & 0x0F00u) >> 8u];
+    // 100s
+    memory[index] = val % 10;
+    //10s
+    memory[index + 1] = (val / 10) % 10;
+    //1s
+    memory[index + 2] = (val / 100) % 10;
+}
+
+// LD [I], Vx, store/write V0 through Vx in memory starting from I
+void chip8::OP_Fx55()
+{
+    for (size_t i = 0; i <= ((opcode & 0x0F00u) >> 8u); ++i)
+    {
+        memory[index+i] = v_registers[i];
+    }
+    
+}
+
+// LD Vx, [I], read V0 through Vx
+void chip8::OP_Fx65()
+{
+    for (size_t i = 0; i <= ((opcode & 0x0F00u) >> 8u); ++i)
+    {
+        v_registers[i] = memory[index+i];
+    }
 }
